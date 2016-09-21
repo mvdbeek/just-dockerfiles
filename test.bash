cb0957cabf97e3ee45a5560c8153b93b4ae6e14f
Marius van den Beek <m.vandenbeek@gmail.com>
2016-09-21 11:06:48 +0200

    Use native python tracebacks to avoid leaking credentials

--- test.bash
+++ test.bash
@@ -8,5 +8,5 @@ cd `dirname $0`
 RECIPE_DIRECTORY="tests/recipes"
 export RECIPE_DIRECTORY
 
-py.test run_test.py -k 'test_test1'
-! py.test run_test.py -k 'test_test2'
+py.test --tb=native run_test.py -k 'test_test1'
+! py.test --tb=native run_test.py -k 'test_test2'
